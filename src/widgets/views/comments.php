c3b9dbb1e1c7220394afd7caa1a9cdd5ae4e8f15
naffiq <galymzhan@rocketfirm.com>
2016-04-25 13:40:53 +0600

    updated widget and behavior

--- src/widgets/views/comments.php
+++ src/widgets/views/comments.php
@@ -1,8 +1,13 @@
+<?php
+/**
+ * @var $comments \rocketfirm\comments\models\Comment[]
+ */
+?>
 <div class="reviews">
     <a id="comments"></a>
-    <h3 class="content-title">Отзывы читателей <span>(34)</span></h3>
+    <h3 class="content-title"><?= Yii::t('frontend', 'Отзывы читателей') ?> <span>(<?= $model->getCommentsCount() ?>)</span></h3>
     <a class="reviews-login" href="">
-        Войти
+        <?= Yii::t('frontend', 'Войти') ?>
         <svg class="icon icon--down">
             <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-down"></use>
         </svg>
@@ -14,6 +19,7 @@
      </select>
  */ ?>
     <div class="reviews-comment-section flex-760">
+
         <?php \yii\widgets\ActiveForm::begin([
             'options' => [
                 'style' => 'width: 100%;'
@@ -24,32 +30,34 @@
             <input class="reviews-comment-input" type="text" placeholder="Ваш комментарий...">
         </div>
         <?php \yii\widgets\ActiveForm::end() ?>
+
         <div class="reviews-comment-row">
-            <div class="reviews-avatar"></div>
-            <div class="reviews-comment-block">
-                <div class="reviews-comment-info">
-                    <div class="reviews-comment-name inline-block">Serik Dossayev</div>
-                    <div class="reviews-comment-time inline-block">12 часов назад</div>
-                    <div class="reviews-likes inline-block">
-                        <a class="reviews-likes-less" href="#">
-                            <i></i>
-                        </a>
-                        <div class="reviews-likes-num reviews-likes-num--plus">+2</div>
-                        <a class="reviews-likes-more" href="#">
-                            <i></i>
-                        </a>
+            <?php foreach($comments as $comment): ?>
+                <div class="reviews-avatar"></div>
+                <div class="reviews-comment-block">
+                    <div class="reviews-comment-info">
+                        <div class="reviews-comment-name inline-block"></div>
+                        <div class="reviews-comment-time inline-block">12 часов назад</div>
+                        <div class="reviews-likes inline-block">
+                            <a class="reviews-likes-less" href="#">
+                                <i></i>
+                            </a>
+                            <div class="reviews-likes-num reviews-likes-num--plus"><?= $comment->rating ?></div>
+                            <a class="reviews-likes-more" href="#">
+                                <i></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="reviews-comment-text plain-text">
+                        <?= $comment->text ?>
+                    </div>
+                    <div class="reviews-comment-actions">
+                        <a class="reviews-comment-link inline-block" href="#"><?= Yii::t('frontend', 'Ответить') ?></a>
+                        <a class="reviews-comment-link inline-block" href="#"><?= Yii::t('frontend', 'Поделиться') ?></a>
                     </div>
                 </div>
-                <div class="reviews-comment-text plain-text">В преддверии выборов решением Центр-избиркома и
-                    апелляционной палаты была снята с предвыборной гонки партия "Патрия", по спискам которой проходил
-                    предприниматель Ренато Усатый. Поводом для этого стали материалы МВД, согласно которым партии якобы
-                    были переведены из-за рубежа около 500 тысяч долла...
-                </div>
-                <div class="reviews-comment-actions">
-                    <a class="reviews-comment-link inline-block" href="#">Ответить</a>
-                    <a class="reviews-comment-link inline-block" href="#">Поделиться</a>
-                </div>
-            </div>
+            <?php endforeach ?>
+
         </div>
     </div>
 </div>
